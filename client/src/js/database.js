23ffb96d34b38f97ec5b7959dcec348b167cae9a
Manoj Adikari <manojpadikari@gmail.com>
2023-05-07 07:51:34 +0930

    Create Database  file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, content: content });
+  const result = await request;
+  console.log("Content saved in IndexedDB:", result);
 
+};
+ 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  console.log("GET to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("Cretrieved from IndexedDB:", result);
+
+  if (result && result.length > 0 && result[0].content) {
+    return result[0].content;
+  } else {
+    console.error("getDb not implemented", result);
+    return null;
+  }
+};
+
 
 initdb();
